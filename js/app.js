ad6ba436159c826d185472c4c567b2abb2a8c489
jberthiaume1138 <joenyc23@gmail.com>
2016-03-17 15:39:14 -0400

    coding clear buttons

--- js/app.js
+++ js/app.js
@@ -6,10 +6,17 @@ $(document).ready(function() {
 	)
 
 	$('#button-clear').click(function() {
-		
 		console.log("button-clear clicked");
-	}
-	);
+		$('#clear').hide();
+		$('#confirm').show();
+	})
+
+	$('#button-confirm').click(function() {
+		console.log("button-confirm clicked");
+		$('#clear').show();
+		$('#confirm').hide();	
+		// clear list handler
+	});
 
 
 
