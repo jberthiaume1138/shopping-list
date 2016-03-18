b9e581abeef36ccd413b8ea5fc3ef9db5afab750
jberthiaume1138 <joenyc23@gmail.com>
2016-03-18 17:41:16 -0400

    styling mostly complete

--- js/app.js
+++ js/app.js
@@ -1,9 +1,14 @@
 $(document).ready(function() {
 	$('#button-add').click(function() {
-		
-		console.log("button-add clicked");
-	}
-	)
+		// console.log("button-add clicked");
+		var newItem = $('#input-item').val();
+		var newListTag = '<li class="list-item">';
+		//var for GotIt <img>
+		//var for X <img>
+		console.log(newItem);
+		$('#list').prepend(newListTag + newItem + "</li>");
+
+	})
 
 	$('#button-clear').click(function() {
 		console.log("button-clear clicked");
