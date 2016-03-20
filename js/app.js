496ebd64691a06cb93412bde04ffe63057234bda
Joseph Berthiaume <joenyc23@gmail.com>
2016-03-20 15:06:58 -0400

    adding javascript

--- js/app.js
+++ js/app.js
@@ -1,14 +1,33 @@
 $(document).ready(function() {
-	$('#button-add').click(function() {
+	$('#button-add').on("click",function() {
 		// console.log("button-add clicked");
 		var newItem = $('#input-item').val();
 		var newListTag = '<li class="list-item">';
+		// var newItem = "Ham";
+		btnAdd(newItem);
+		// console.log("clicked");
+		// btnAdd(newItem);
 		//var for GotIt <img>
 		//var for X <img>
-		console.log(newItem);
-		$('#list').prepend(newListTag + newItem + "</li>");
-
 	})
+
+	$('#input-item').keydown(function(e) {
+		console.log(e.keyCode);
+		if (e.keyCode == 13) {
+			// call function to add item to the list
+		}
+	})
+
+	
+
+	$('#input-item').click(function(e) {
+		console.log($(this).val());
+		// doStuff(e);
+	})
+
+
+
+
 
 	$('#button-clear').click(function() {
 		console.log("button-clear clicked");
@@ -17,14 +36,28 @@ $(document).ready(function() {
 	})
 
 	$('#button-confirm').click(function() {
+		// handler to clear the list
 		console.log("button-confirm clicked");
+		// $('#list').empty();
 		$('#clear').show();
 		$('#confirm').hide();	
-		// clear list handler
+		//this is destroying the <ul>, so it would have to be re-created for new items...
+		//need logic for insertion
+		var instructions = "<p>Enter your items, one at a time in the box above, then click Add or press Enter.</p>";
+		$('#list').replaceWith(instructions);
 	});
 
+// function doStuff(e) {
+// 	console.log("Stuff from box = " + $(this).val());
+// };
 
 
+function btnAdd (newItem) {
+	console.log("clicked");
+	console.log(newItem);
+	// $('#list').prepend(newListTag + newItem + "</li>");
+	// $('#list:first').prepend('<p>IMAGE</p>');
+};
 
 
 
